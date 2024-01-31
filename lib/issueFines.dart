4f96815b930c0f59383e8d837efd205fd41a0162
Shanka Visal <shankavisal@gmail.com>
2024-01-31 15:58:33 +0530

    commit1

--- lib/issueFines.dart
+++ lib/issueFines.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 class issueFines extends StatefulWidget {
-  const issueFines({super.key});
 
   @override
   State<issueFines> createState() => _issueFinesState();
@@ -9,31 +8,40 @@ class issueFines extends StatefulWidget {
 class _issueFinesState extends State<issueFines> {
   late double screenWidth;
   late double screenHeight;
+  int total = 0;
   bool? isChecked1 = false;
+  bool? isChecked2 = false;
+  bool? isChecked3 = false;
+  bool? isChecked4 = false;
+  bool? isChecked5 = false;
+  bool? isChecked6 = false;
+  bool? isChecked7 = false;
+  bool? isChecked8 = false;
+  bool? isChecked9 = false;
 
   @override
   Widget build(BuildContext context) {
-    screenWidth = MediaQuery.of(context).size.width;
+   screenWidth = MediaQuery.of(context).size.width;
    screenHeight = MediaQuery.of(context).size.height;
 
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Issue Fines'),
+        title: const Text('Issue Fines'),
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(8),
+          padding: const EdgeInsets.all(8),
           child: Center(
             child: Column(
               children: [
                 Container(
                   width: screenWidth,
                   height: screenHeight/20,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                     color: Color(0xff424750),
                   ),
-                  child: Center(
+                  child: const Center(
                     child: Text(
                       'Select the violation type',
                       style: TextStyle(
@@ -43,11 +51,14 @@ class _issueFinesState extends State<issueFines> {
                     ),
                   ),
                 ),
+                _checkboxRow("Driving without a  valid license - LKR 100.00", isChecked1),
+                _checkboxRow("Driving without a  valid license - LKR 200.00", isChecked2),
+                _checkboxRow("Driving without a  valid license - LKR 300.00", isChecked3),
                 Row(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget> [
-                    Text("Driving without a  valid license - LKR 10,000.00"),
+                    const Text("Driving without a  valid license - LKR 100.00"),
                     Checkbox(
                       //tristate: true,
                       value: isChecked1,
@@ -59,70 +70,17 @@ class _issueFinesState extends State<issueFines> {
                     ),
                   ],
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget> [
-                    Text("Driving without a  valid license - LKR 10,000.00"),
-                    Checkbox(
-                      //tristate: true,
-                      value: isChecked1,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked1 = value;
-                        });
-                      },
-                    ),
-                  ],
+
+                ElevatedButton(
+                    onPressed: (){
+                      if(isChecked1 == true) total = total+100;
+                      if(isChecked2 == true) total = total+200;
+                      if(isChecked3 == true) total = total+300;
+                      setState(() {});
+                    },
+                    child: Text('Total fine $total'),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget> [
-                    Text("Driving without a  valid license - LKR 10,000.00"),
-                    Checkbox(
-                      //tristate: true,
-                      value: isChecked1,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked1 = value;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget> [
-                    Text("Driving without a  valid license - LKR 10,000.00"),
-                    Checkbox(
-                      //tristate: true,
-                      value: isChecked1,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked1 = value;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget> [
-                    Text("Driving without a  valid license - LKR 10,000.00"),
-                    Checkbox(
-                      //tristate: true,
-                      value: isChecked1,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          isChecked1 = value;
-                        });
-                      },
-                    ),
-                  ],
-                )
+
           
               ],
             ),
@@ -131,5 +89,25 @@ class _issueFinesState extends State<issueFines> {
       ),
     );
   }
+
+  Widget _checkboxRow(String text, bool? isChecked){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget> [
+        Text(text),
+        Checkbox(
+          //tristate: true,
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
 }
 
