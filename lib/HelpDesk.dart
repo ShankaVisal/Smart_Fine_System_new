f1ea4be57d61751e8aae69dbebb55fea5d92a9fa
Dinesh Manchanayaka <mail.dineshmadusanka@gmail.com>
2024-04-08 17:43:25 +0530

    Help Desk Page.
    v 1.0

--- lib/HelpDesk.dart
+++ lib/HelpDesk.dart
@@ -1,10 +1,92 @@
 import 'package:flutter/material.dart';
 
-class HelpDesk extends StatelessWidget {
-  const HelpDesk({super.key});
+class Support extends StatelessWidget {
+  Support({Key? key}) : super(key: key);
+
+  final List<String> steps = [
+    "Scan QR Code",
+    "Tap Check Vehicle",
+    "Enter Vehicle Number",
+    "Tap Next",
+    "Select Issue Fine or Menu",
+    "Select Violation types on checkboxes if there is any!",
+    "Select Next",
+    "Check Confirmation",
+    "Issue Fine!"
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: const Color(0xFF005EA3),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(255, 44, 44, 44).withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                // changes position of shadow
+              ),
+            ],
+          ),
+          child: GestureDetector(
+            child: const Icon(Icons.arrow_back),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Support",
+            style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            height: screenHeight / 5 * 3,
+            child: Expanded(
+              child: ListView.builder(
+                itemCount: steps.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Text((index + 1).toString()),
+                        ),
+                        title: Text(
+                          steps[index],
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        onTap: () {
+                          // Handle tap if needed
+                        },
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
